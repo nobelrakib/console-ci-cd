6a8e3aaaf1139f80e6bacddbd7bf08549d5ecd5c
nobelrakib03 <nobelrakib>
2024-09-26 13:30:44 +0800

    myconsoleapp release path

--- src/MyConsoleApp/MyConsoleApp/obj/Release/net6.0/MyConsoleApp.AssemblyInfo.cs
+++ src/MyConsoleApp/MyConsoleApp/obj/Release/net6.0/MyConsoleApp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c36d2c490b0004ae5c21394c3eb4140ed9d771")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
